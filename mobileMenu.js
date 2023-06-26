0ce48f611585b16d5d0ea63d95564e71fc1230c9
Zuwairanajma <juwairiyyasadiq@gmail.com>
2023-06-26 15:19:52 +0100

    Run linters setup files

--- mobileMenu.js
+++ mobileMenu.js
@@ -1,23 +1,21 @@
-let bars= document.querySelector('.fa-bars');
-   let list= document.querySelector('.menu-list')
-   let close = document.querySelector('.close');
-   const navItems = document.querySelectorAll('.menu-list ul li');
+const bars = document.querySelector('.fa-bars');
+const list = document.querySelector('.menu-list');
+const close = document.querySelector('.close');
+const navItems = document.querySelectorAll('.menu-list ul li');
 
-   
 const showMenu = () => {
-    bars.style.display ="none";
-    close.style.transform = "translateY(0)";
-    list.style.transform= "translateY(0)";
+  bars.style.display = 'none';
+  close.style.transform = 'translateY(0)';
+  list.style.transform = 'translateY(0)';
+};
+const hideMenu = () => {
+  list.style.transform = 'translateX(-100%)';
+  bars.style.display = 'block';
+  close.style.transform = 'translateX(-25rem)';
+};
 
-}
-  const hideMenu = () => {
-    list.style.transform = "translateX(-100%)";
-    bars.style.display = "block";
-    close.style.transform = "translateX(-25rem)";
-}
-
-  bars.addEventListener('click', showMenu);
-  close.addEventListener("click", hideMenu);
-  navItems.forEach((item) => {
-    item.addEventListener('click', hideMenu)
- });
+bars.addEventListener('click', showMenu);
+close.addEventListener('click', hideMenu);
+navItems.forEach((item) => {
+  item.addEventListener('click', hideMenu);
+});
